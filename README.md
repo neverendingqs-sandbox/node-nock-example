bf99dbc3ee401044038b5e226a90a6b499201776
Mark Tse <mark.tse@neverendingqs.com>
2017-03-11 10:53:42 -0500

    aftereach-isdone - example init.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # node-nock-example
+
+All examples are for [nock](https://github.com/node-nock/nock) using [mocha](https://mochajs.org/).
+
+## Using isDone() and cleanAll() in afterEach()
+
+Include the following to ensure `nock` interceptors don't affect other tests (and make debugging hard):
+
+```js
+afterEach(function() {
+  ...
+  if(!nock.isDone()) {
+    this.test.error(new Error('Not all nock interceptors were used!'));
+    nock.cleanAll();
+  }
+  ...
+});
+```
+
+See the difference in action:
+
+```bash
+npm test test/aftereach-isdone/server-original.js
+npm test test/aftereach-isdone/server-improved.js
+```
---
+++ test/aftereach-isdone/server-original.js
@@ -0,0 +1,40 @@
+'use strict';
+const assert = require('chai').assert;
+const nock = require('nock');
+const request = require('supertest');
+const uniqueValue = 'da64daaf-182b-4af6-a4af-09727bf8d5aa';
+
+const app = require('../../src/server');
+
+describe('server-original', function() {
+  it('GET / once', function() {
+    nock('https://www.example.com')
+      .get('/')
+      .reply(200, uniqueValue);
+
+    return request(app)
+      .get('/')
+      .then(res => assert.equal(res.text, uniqueValue));
+  });
+
+  it('GET / five times', function() {
+    nock('https://www.example.com')
+      .get('/')
+      .times(5)
+      .reply(200, uniqueValue);
+
+    return request(app)
+      .get('/')
+      .then(res => assert.equal(res.text, uniqueValue));
+  });
+
+  it('GET / fails if example.com is down', function() {
+      nock('https://www.example.com')
+        .get('/')
+        .reply(500);
+
+      return request(app)
+        .get('/')
+        .then(res => assert.equal(res.status, 503));
+    });
+});
